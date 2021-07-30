c5cc0f08575318df00b9c2a36aa56db25ab3ca87
ploranger8 <87283134+ploranger8@users.noreply.github.com>
2021-07-30 11:35:14 -0400

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+print ('Hello, I am Willie and I am a Chatbot')
+print ('I like animals and I love to talk about food.')
+name = input ('What is your name: ')
+print ('Hello', name,', nice to meet you.')
+year = input ('I lost track of time, what year is it?: ')
+print ('Yes, that sounds right, thank you!')
+#Math fun with variables!
+my_age = input ("Let's play a game, try to guess my age: ")
+print ('You got it! I am ',my_age)
+my_age = int(my_age)
+nyears = 100-my_age
+print ('Did you know that I will be 100 in', nyears, 'years?')
+print ('That will be in the year', nyears + int(year))
+#Food Chat
+print ('I love sushi but also like to try new things.')
+food = input ('What is your favorite food?: ')
+print ('Yum, I like ', food, ' too.')
+question = ('How often do you eat ' + food + '?: ')
+howoften = input(question)
+print ('Interesting, I wonder if eating',food, howoften, 'is good for your health.')
+#About the animals
+animal = input ('My favority animal is a Capybara, what is yours?: ')
+print (animal, '! I do not like them.')
+print ('I wonder if a',animal, 'likes to eat',food, '?')
+#checking the temp
+feeling = input ('How are you feeling today?: ')
+print ('Why are you feeling', feeling, 'now?')
+reason = input ('Please tell me: ')
+print ('I understand, ', reason, 'makes me feel',feeling, 'too. Thanks, for sharing.')
+#Willie the Chatbot signing off
+print ('It was nice talking to you but it has been a long day.')
+print ('I have to go play video games now!')
+print ('I hope we can chat again later.')
